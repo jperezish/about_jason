8dc2d461e3b2318b7ab26b4f30598e11ffa14f40
jason perez <jperezish@gmail.com>
2014-02-14 13:58:16 -0800

    update about

--- lib/about_jason/about.rb
+++ lib/about_jason/about.rb
@@ -1,3 +1,7 @@
-puts "About Jason"
-puts "----------------------"
-puts "My name is Jason and I like bacon."
+class Jason
+  def self.about
+    puts "About Jason"
+    puts "----------------------"
+    puts "My name is Jason and I like bacon."
+  end
+end
